03b977ae326e194c1658cd7a60600d62fe0ffb0c
Peter K <peteryunkim@gmail.com>
2017-03-14 11:05:44 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+	def name=(name_given)
+		@name = name_given
+	end
+
+	def name
+		@name
+	end
+
+	def breed=(breed_type)
+		@breed = breed_type
+	end
+
+	def breed
+		@breed
+	end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+	def name=(name_given)
+		@name = name_given
+	end
+
+	def name
+		@name
+	end
+
+	def job=(job_earned)
+		@job = job_earned
+	end
+
+	def job
+		@job
+	end
+end
